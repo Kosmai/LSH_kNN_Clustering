170b2c91a6a5ff2b6e464958f17099848a1b586b
Kosmai <kosmaikosmai@gmail.com>
2021-12-03 14:05:05 +0200

    Merged LSH/Cube main

---
+++ Time_Series_kNN_Clustering/src/search.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <random>
+#include <map>
+#include "../inc/randGen.hpp"
+#include "../inc/point.hpp"
+#include "../inc/LSH.hpp"
+#include "../inc/hypercube.hpp"
+#include "../inc/searchUtils.hpp"
+
+int main(int argc, char **argv) {
+
+	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+	setRandomSeed(seed);
+
+	std::string inputFile  = "datasets/nasdaq2017_LQ.csv";
+	std::string queryFile  = "datasets/query.csv";
+	std::string outputFile = "out.txt";
+
+	int dims;
+	int k = 4;
+	int l = 5;
+	int m = 1000;
+    int probes = 500;
+	int numOfNearest = 5;
+	double radius = 0.0;
+	double w;
+
+	LSH* lsh = nullptr;
+	Hypercube* cube = nullptr;
+
+	std::vector<Point*> points;
+
+	initializeInput(inputFile, dims, points);
+
+	lsh = initializeLSH(dims, l, k, w, points);
+	cube = initializeHypercube(dims, k, w, points);
+
+	printParameters(k, l, numOfNearest, radius, w);
+
+	if(searchLoop(*lsh, queryFile, outputFile, numOfNearest, radius) < 0) return 2;
+	if(searchLoop(*cube, queryFile, outputFile, m, probes, numOfNearest, radius) < 0) return 2;
+
+	//free all memory
+	for(auto point: points){
+		delete point;
+	}
+	if(lsh != nullptr){
+		delete lsh;
+	}
+	if(cube != nullptr){
+		delete cube;
+	}
+	return 0;
+}
---
+++ Time_Series_kNN_Clustering/src/searchUtils.cpp
@@ -0,0 +1,226 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <random>
+#include <map>
+#include "../inc/hashFunctionG.hpp"
+#include "../inc/randGen.hpp"
+#include "../inc/hashTable.hpp"
+#include "../inc/point.hpp"
+#include "../inc/LSH.hpp"
+#include "../inc/hypercube.hpp"
+#include "../inc/readInput.hpp"
+#include "../inc/searchUtils.hpp"
+
+#define W_LSH_MULTIPLIER 0.1
+#define W_CUBE_MULTIPLIER 0.2
+#define BUCKET_DIVISOR 4
+
+
+Hypercube* initializeHypercube(int dims, int k, double &w, std::vector<Point*> &points){
+	//initialize w
+    std::cout << "Calculating optimal w...";
+    w = Hypercube::calculateW(points) * W_CUBE_MULTIPLIER;
+    std::cout << "done" << std::endl;
+
+    //Hypercube initialize will all points in inputfile
+    std::cout << "Generating required structures...";
+	fflush(stdout);
+    Hypercube* cube = new Hypercube(dims, pow(2, k), 1, k, w);
+    for(auto point: points){
+        cube->addPoint(point);
+    }
+    std::cout << "done" << std::endl;
+	return cube;
+}
+
+LSH* initializeLSH(int dims, int l, int k, double &w, std::vector<Point*> &points){
+	//initialize buckets and w
+	std::cout << "Calculating optimal w...";
+	fflush(stdout);
+	w = LSH::calculateW(points)*W_LSH_MULTIPLIER;
+	int buckets = points.size() >= BUCKET_DIVISOR ? points.size()/BUCKET_DIVISOR : 1;
+	std::cout << "done" << std::endl;
+
+	//LSH initialize will all points in inputfile
+	std::cout << "Generating required structures...";
+	fflush(stdout);
+	LSH* lsh = new LSH(dims, buckets, l, k, w);
+    for(auto point: points){
+        lsh->addPoint(point);
+    }
+	std::cout << "done" << std::endl;
+	return lsh;
+}
+
+int initializeInput(std::string &inputFile, int &dims, std::vector<Point*> &points){
+
+	//read cli arguments
+	// if (readLshArguments(argc, argv, inputFile, queryFile, k, l, outputFile, numOfNearest, radius) != 0) {
+	// 	std::cout << "Unknown argument read! Aborting..." << std::endl;
+	// 	return -1;
+	// }
+	
+	if(inputFile == ""){
+		std::cout << "Give path to input file: "; 
+		std::cin >> inputFile;
+	}
+	
+	std::cout << "Reading and analyzing input file...";
+	fflush(stdout);
+	if((dims = readDataSet(inputFile, '\t', points)) < 0){
+		std::cout << "Error while reading input file. Aborting..." << std::endl;
+		return -1;
+	}
+	std::cout << "done" << std::endl;
+	return 0;
+}
+
+void printParameters(int k, int l, int numOfNearest, double radius, int w){
+	std::cout << "--------------------------------------------------" << std::endl;
+	std::cout << "Parameters" << std::endl;
+	std::cout << "--------------------------------------------------" << std::endl;
+	std::cout << "k: " << k << std::endl;
+	std::cout << "l: " << l << std::endl;
+	std::cout << "N: " << numOfNearest << std::endl;
+	std::cout << "R: " << radius << std::endl;
+	std::cout << "w: " << w << std::endl;
+	std::cout << "--------------------------------------------------" << std::endl;
+	return;
+}
+
+int searchLoop(LSH& lsh, std::string queryFile, std::string outputFile, int numOfNearest, double radius){
+
+	std::vector<Point*> queries;
+
+	//main program loop
+	while(true){
+		if(queryFile == ""){
+			std::cout << "Give path to query file or \"exit\" to terminate: "; 
+			std::cin >> queryFile;
+			if(queryFile == "exit"){
+				break;
+			}
+		}
+		if(outputFile == ""){
+			std::cout << "Give path to output file: "; 
+			std::cin >> outputFile;
+		}
+
+		//prints all parameters
+		std::cout << "--------------------------------------------------" << std::endl;
+		std::cout << "Files" << std::endl;
+		std::cout << "--------------------------------------------------" << std::endl;
+		std::cout << "queryFile: " << queryFile << std::endl;
+		std::cout << "outputFile: " << outputFile << std::endl;
+		std::cout << "--------------------------------------------------" << std::endl;
+		std::cout << "Results" << std::endl;
+		std::cout << "--------------------------------------------------" << std::endl;
+
+		//read query file
+		if(readDataSet(queryFile, '\t', queries) < 0){
+			std::cout << "Error while reading query file. Aborting..." << std::endl;
+			return -1;
+		}
+
+		//open output file
+		FILE* outfp = fopen(outputFile.c_str(),"w");
+		if(outfp == nullptr){
+            std::cout << "Could not open output file. Aborting..." << std::endl;
+            return -2;
+        }
+
+		//run the algorithm
+		for(unsigned int i = 0; i < queries.size(); i++){
+			lsh.calculateNN(*queries[i], outfp, numOfNearest, radius);
+		}
+
+		//statistics
+		if(lsh.successfulQueries > 0){
+			std::cout << "Statistics" << std::endl;
+			std::cout << "--------------------------------------------------" << std::endl;
+			std::cout << "Average predicted/true distance ratio: " << lsh.averageRatio/lsh.successfulQueries << std::endl;
+			std::cout << "Worst   predicted/true distance ratio: " << lsh.worstDistance << std::endl;
+			std::cout << "--------------------------------------------------" << std::endl;
+		}
+		
+		//cleanup - reset
+		for(auto point: queries){
+			delete point;
+		}
+		queries.clear();
+		queryFile = "";
+		outputFile = "";
+		lsh.resetStatistics();
+		fclose(outfp);
+	}
+	return 0;
+}
+
+int searchLoop(Hypercube& cube, std::string queryFile, std::string outputFile, int probes, int m, int numOfNearest, double radius){
+
+	std::vector<Point*> queries;
+
+	//main program loop
+	while(true){
+		if(queryFile == ""){
+			std::cout << "Give path to query file or \"exit\" to terminate: "; 
+			std::cin >> queryFile;
+			if(queryFile == "exit"){
+				break;
+			}
+		}
+		if(outputFile == ""){
+			std::cout << "Give path to output file: "; 
+			std::cin >> outputFile;
+		}
+
+		//prints all parameters
+		std::cout << "--------------------------------------------------" << std::endl;
+		std::cout << "Files" << std::endl;
+		std::cout << "--------------------------------------------------" << std::endl;
+		std::cout << "queryFile: " << queryFile << std::endl;
+		std::cout << "outputFile: " << outputFile << std::endl;
+		std::cout << "--------------------------------------------------" << std::endl;
+		std::cout << "Results" << std::endl;
+		std::cout << "--------------------------------------------------" << std::endl;
+
+		//read query file
+		if(readDataSet(queryFile, '\t', queries) < 0){
+			std::cout << "Error while reading query file. Aborting..." << std::endl;
+			return -1;
+		}
+
+		//open output file
+		FILE* outfp = fopen(outputFile.c_str(),"w");
+		if(outfp == nullptr){
+            std::cout << "Could not open output file. Aborting..." << std::endl;
+            return -2;
+        }
+
+		//run the algorithm
+		for(unsigned int i = 0; i < queries.size(); i++){
+			cube.calculateNN(*queries[i], outfp, m, probes, numOfNearest, radius);
+		}
+
+		//statistics
+		if(cube.successfulQueries > 0){
+			std::cout << "Statistics" << std::endl;
+			std::cout << "--------------------------------------------------" << std::endl;
+			std::cout << "Average predicted/true distance ratio: " << cube.averageRatio/cube.successfulQueries << std::endl;
+			std::cout << "Worst   predicted/true distance ratio: " << cube.worstDistance << std::endl;
+			std::cout << "--------------------------------------------------" << std::endl;
+		}
+		
+		//cleanup - reset
+		for(auto point: queries){
+			delete point;
+		}
+		queries.clear();
+		queryFile = "";
+		outputFile = "";
+		cube.resetStatistics();
+		fclose(outfp);
+	}
+	return 0;
+}
